1934fecc2bce7211c35852cf5c216841305fb932
salac <salac@ubuntu.(none)>
2016-12-11 15:37:49 -0500

    refactoring6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,23 +21,24 @@ RSpec.describe User, type: :model do
  	let!(:user) { create(:user) }
  	let!(:user2) { create(:user, email: 'pera@gmail.com') }
  	let!(:schedule) { 
-    	create(:schedule, hours: 4, 
+    	create(:schedule, hours: 20, 
     	user: user ) 
     }    
-    let!(:schedule2) { 
-    	create(:schedule, action: 'Sick leave', 
-        user: user2) 
-    }    
+
+    let!(:schedules) { create_list(:schedule, 10, 
+    	action: 'Sick leave', hours: 0, user: user2) }
 
     context "executes methods correctly" do
       context "top_overtime?" do
         it "top_overtime? does what it's supposed to..." do
           expect(user.top_overtime?).to eq(true) 
+          expect(user2.top_overtime?).to eq(false) 
      	end
       end
 
       context "overused_sick_leave?" do
-        it "overused_sick_leave? does what it's supposed to..." do     
+        it "overused_sick_leave? does what it's supposed to..." do    
+          expect(user2.overused_sick_leave?).to eq(true) 
           expect(user.overused_sick_leave?).to eq(false)
      	end
       end
