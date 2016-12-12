14a06628373979d4e8aa567ef1c3236a15bb1057
salac <salac@ubuntu.(none)>
2016-12-12 16:19:25 -0500

    refactoring12

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,27 +5,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  scope :year_sick_leave, -> (user_id) { 
-    joins(:schedules)
-   .where("schedules.created_at > ? AND schedules.created_at < ? AND user_id = ? 
-      AND action = 'Sick leave'", Time.now.beginning_of_year, Time.now.end_of_year, user_id)
-   .count
-  }
-
-  scope :top_overtime, -> { 
-      joins(:schedules)
+  def top_overtime?
+     user = User.joins(:schedules)
      .select("users.*, count(schedules.action == 'Overtime') as ocount")
      .order("ocount DESC")
      .first
-  }
-
-  def top_overtime?
-     user = User.top_overtime
      user.id == self.id unless user.nil?
   end
 
   def overused_sick_leave?
-	  User.year_sick_leave(self.id) > 4
+	  User.joins(:schedules)
+    .where("schedules.created_at > ? AND schedules.created_at < ? AND user_id = ? 
+      AND action = 'Sick leave'", Time.now.beginning_of_year, Time.now.end_of_year, self.id)
+    .count > 4
   end
 
 end
