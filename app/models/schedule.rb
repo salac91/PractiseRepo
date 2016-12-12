8ed17942472203d2b01665dad077e7e06898686f
salac <salac@ubuntu.(none)>
2016-12-12 12:50:28 -0500

    refactoring8

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -5,7 +5,7 @@ class Schedule < ApplicationRecord
 	validates :date, presence: true
 	validates :user_id, presence: true, numericality: true
 
-	before_save { self.hours *= 1.5 if self.hours == 'Overtime' } 
+	before_save { self.hours *= 1.5 if self.action == 'Overtime' } 
 
 	scope :monthly_overtimes, -> { where("created_at > ? AND created_at < ? AND action = 'Overtime' ", 
 		  Time.now.beginning_of_month, Time.now.end_of_month)
